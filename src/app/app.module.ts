d56a6a5899b624d118bed278f17e9fd86d350dfe
Oscar Fernando Muñoz Gayoso <oscarmg_4@outlook.com>
2022-12-02 12:14:47 -0500

    1er Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+
+//Crear un modulo ContadoresModule
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadoresModule } from './contadores/contadores.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadoresModule
   ],
   providers: [],
   bootstrap: [AppComponent]
